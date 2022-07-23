def532dbb9c44ea234e640499faed4413d6ce845
Rafael Vieira <rafaeldevcode@gmail.com>
2022-07-23 18:09:19 -0300

    <refactor>[Policy end terms]: Separando o body da página de termos e politicas em um componente separado e adicionando ícone nos bradcamps

--- frontend/src/screens/policies/privacyScreen/index.js
+++ frontend/src/screens/policies/privacyScreen/index.js
@@ -12,15 +12,17 @@ export default function PrivacyScreen(){
                 <section className='w-100'>
                     <Header />
 
-                    <article className='p-3'>
-                        <Breadcrumbs type='Ver' color='cm-success' title='Politicas de Privacidade' bread={{
+                    <section className='p-3'>
+                        <Breadcrumbs type='Ver' color='cm-success' title='Politicas de Privacidade' icon='bi bi-file-earmark-text-fill' bread={{
                             '1':'&gt;',
                             '2': 'policies',
                             '3': '&gt;',
                             '4': 'privacy'
                         }} />
+                    </section>
 
-                        <div className='p-3 bg-cm-grey shadow'>
+                    <article className='p-3 m-3 bg-cm-grey shadow'>
+                        <div>
                             <p>
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                             </p>
--- frontend/src/screens/policies/termsScreen/index.js
+++ frontend/src/screens/policies/termsScreen/index.js
@@ -12,15 +12,17 @@ export default function TermsScreen(){
                 <section className='w-100'>
                     <Header />
 
-                    <article className='p-3'>
-                        <Breadcrumbs type='Ver' color='cm-success' title='Termos de Uso' bread={{
+                    <section className='p-3'>
+                        <Breadcrumbs type='Ver' color='cm-success' title='Termos de Uso' icon='bi bi-file-earmark-text-fill' bread={{
                             '1':'&gt;',
                             '2': 'policies',
                             '3': '&gt;',
                             '4': 'terms'
                         }} />
+                    </section>
 
-                        <div className='p-3 bg-cm-grey shadow'>
+                    <article className='p-3 m-3 bg-cm-grey shadow'>
+                        <div>
                             <p>
                                 Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
                             </p>
