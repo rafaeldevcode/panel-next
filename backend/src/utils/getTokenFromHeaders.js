571bda9302f68b8714ac4011e0ebef9d9c5f8718
Rafael Vieira <rafaeldevcode@gmail.com>
2022-07-24 09:26:51 -0300

    <refactor>[Files]: Reorganizando arquivos do projeto

--- backend/src/utils/getTokenFromHeaders.js
+++
@@ -1,5 +0,0 @@
-export function getTokenFromHeaders(req) {
-  const authHeader = req.headers['x-authorization'] || req.headers['authorization'] || '';
-  const token = authHeader?.split(' ')[authHeader?.split(' ').length - 1];
-  return token;
-}
