b4db193f7bf87ce6be633563a215aa482e61d548
Rafael Vieira <rafaeldevcode@gmail.com>
2022-09-11 12:40:23 -0300

    <style>[Button Notifications]: Alterando estilo do botão de abrir as notificação

--- src/partials/message/index.js
+++ src/partials/message/index.js
@@ -1,6 +1,6 @@
 export default function Message({ text, type }, ...props){
     return (
-        <div {...props} className={`position-fixed end-0 top-0 m-2 p-2 border border-cm-${type} border-4 rounded`}>
+        <div {...props} className={`position-fixed end-0 top-0 m-2 p-2 border border-cm-${type} border-4 rounded shadow`}>
             <p className={`m-0 text-cm-${type}`}>{text}</p>
         </div>
     )
--- src/partials/notifications/index.js
+++ src/partials/notifications/index.js
@@ -3,7 +3,7 @@ import { oppenNotifications } from "../../services/oppenNotifications";
 export default function Notifications({ notifications }){
     return (
         <div id="notifications" className='position-fixed bottom-0 end-0 notification border border-cm-primary bg-cm-light p-2 rounded w-100'>
-            <button title="Abrir notificações" onClick={oppenNotifications} className='border border-cm-secondary border-start border-top border-end notification-btn position-absolute bg-cm-primary end-0 rounded-top p-1 d-flex justify-content-center align-items-center'>
+            <button title="Abrir notificações" onClick={oppenNotifications} className='border border-cm-secondary notification-btn position-absolute bg-cm-primary end-0 rounded-circle p-1 d-flex justify-content-center align-items-center'>
                 <i className='bi bi-bell-fill fs-4 text-cm-light'>
                     <i className='bi bi-circle-fill text-cm-danger position-absolute top-0 end-0 mt-1 me-1' />
                 </i>
